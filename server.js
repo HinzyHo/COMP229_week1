dfb9c99f034bbe4c0284492550187e94570eb722
HinzyHo <45452993+HinzyHo@users.noreply.github.com>
2022-05-26 20:26:52 -0400

    pages fulfill
    file construction change;
    index page + user page

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express")
 const app = new express()
 
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.send('Home')
+})
+
+app.get('/hello', function(req, res){
+    res.send('hello')
+})
+app.get('/about', function(req, res) {
+    res.send('about')
 })
 app.listen(8080)
